f528d0a3837b9be1dcc203daf792e9d4f0d678cd
kunalkrishnani <94007218+kunalkrishnani@users.noreply.github.com>
2023-04-21 10:00:39 +0530

    Add files via upload

---
+++ Java_project/src/CustomTools.java
@@ -0,0 +1,61 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class CustomTools {
+    public static JLabel loadImage(String resource){
+        BufferedImage image;
+        try{
+            InputStream inputStream = CustomTools.class.getResourceAsStream(resource);
+            image = ImageIO.read(inputStream);
+            return new JLabel(new ImageIcon(image));
+        }catch(Exception e){
+            System.out.println("Error: " + e);
+        }
+        return null;
+    }
+
+    public static void updateImage(JLabel imageContainer, String resource){
+        BufferedImage image;
+        try{
+            InputStream inputStream = CustomTools.class.getResourceAsStream(resource);
+            image = ImageIO.read(inputStream);
+            imageContainer.setIcon(new ImageIcon(image));
+        }catch(IOException e){
+            System.out.println("Error: " + e);
+        }
+    }
+
+    public static Font createFont(String resource){
+        String filePath = CustomTools.class.getClassLoader().getResource(resource).getPath();
+
+        if(filePath.contains("%20")){
+            filePath = filePath.replaceAll("%20", " ");
+        }
+
+        try{
+            File customFontFile = new File(filePath);
+            Font customFont = Font.createFont(Font.TRUETYPE_FONT, customFontFile);
+            return customFont;
+        }catch(Exception e){
+            System.out.println("Error: " + e);
+        }
+        return null;
+    }
+
+    public static String hideWords(String word){
+        String hiddenWord = "";
+        for(int i = 0; i < word.length(); i++){
+            if(!(word.charAt(i) == ' ')){
+                hiddenWord += "*";
+            }else{
+                hiddenWord += " ";
+            }
+        }
+        return hiddenWord;
+    }
+}
